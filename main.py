1c1af94a4e50eda5a06feb253af96f48fb004f79
kartikkalel2010 <its.kartikkalel@gmail.com>
2025-06-11 21:46:01 +0530

    color change

--- main.py
+++ main.py
@@ -2,11 +2,21 @@ import cv2
 
 image = cv2.imread('368938.jpg')
 
-cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-cv2.resizeWindow('image', 800,500)
+gray_Image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('image', image)
-cv2.waitKey(0)
+resized_Image = cv2.resize(gray_Image,(244,224))
+
+cv2.imshow('Processed Image', resized_Image)
+
+key = cv2.waitKey(0)
+
+if key == ord('s'):
+    cv2.imwrite('grayscale_resized_image.jpg', resized_Image)
+    print("Image saved successfully!")
+else:
+    print("Image not saved.")
+
 cv2.destroyAllWindows()
 
-print(f"Image Dimension: [image.shape]")
+print(f"Processed Image Dimension:{resized_Image.shape}")
+
